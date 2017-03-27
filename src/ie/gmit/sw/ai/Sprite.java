056ad9c438446518f52602ac4e504cac6a9409a0
Will Hogan <willhogan11@hotmail.com>
2017-03-27 17:34:34 +0100

    Added extra lines of code and JFuzzy logic jar to project

--- src/ie/gmit/sw/ai/Sprite.java
+++ src/ie/gmit/sw/ai/Sprite.java
@@ -7,6 +7,8 @@ public class Sprite {
 	private String name; //The name of this sprite
 	private BufferedImage[] frames; //The set of image frames to animate
  	private int index = 0; //Initial starting index in array
+ 	private double lifeForce = 500; // For example
+ 	
  	
 	public Sprite(String name, String... images) throws Exception{
 		this.name = name;
@@ -17,7 +19,35 @@ public class Sprite {
 			frames[i] = ImageIO.read(new java.io.File(images[i])); //Read in each image as a BufferedImage
 		}
 	}
+
 	
+	
+	// This is the Work in progress section of code that John was working on during the Project review.  
+	
+	/*public double engage(double weapon, double anger){
+		NeuralNetwork nn = NeuralNetworkFactory().getInstance().getNetwork("engage");
+		double[] inputs = {weapon, anger};
+		double result = nn.process(inputs);
+		lifeForce += result;
+		
+		FIS fis = FuzzyLogicFactory().getInstance().getFIS("engage");
+		double result1 = fis.evaluate(weapon, anger);
+		lifeForce += result;
+		
+		return lifeForce;
+	}*/
+
+	
+	
+	public boolean isAlive(){
+		
+		return lifeForce > 0;
+	}
+	
+	
+	
+	// We could get the spider moving to the next position here, Thread.sleep(10) 
+	// Use Thread Executor service to manage the threading......
 	public BufferedImage getNext(){ //Returns the next image frame
 		int idx = index;
 		if (index < frames.length - 1){
